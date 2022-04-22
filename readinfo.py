ba47d4ef526716708d0bc4b440526ade4d098e2c
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2022-04-22 08:53:54 +0530

    Add files via upload

---
+++ readinfo.py
@@ -0,0 +1,6 @@
+#program to store information using list
+a=["Saloni Malviya","Dehri,Kukshi,Dist Dhar, MP","7224059545","salonimalviya03@gmail.com"]
+print("Name:",a[0])
+print("Address:",a[1])
+print("Contact No.:",a[2])
+print("Email:",a[3])
