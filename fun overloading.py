14b0aa67eb976642b239b29804adc7212c4ebc5b
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2022-04-22 08:49:39 +0530

    Add files via upload

---
+++ fun overloading.py
@@ -0,0 +1,12 @@
+class A:
+    def random(self):
+        print("I am in class A")
+class C:
+    def random(self):
+        print("I am in class C")
+class B:
+    def random(self):
+        print("I am in class B")
+
+val=B()
+val.random()
