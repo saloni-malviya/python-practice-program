ba1e1fe3c7fc565b96d309edfa996441b26b76cd
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2022-04-22 08:48:27 +0530

    Add files via upload

---
+++ createdict.py
@@ -0,0 +1,9 @@
+#program to create dictonary
+dict={"saloni":"nikita"}
+print(dict)
+
+#update the dictonary
+dict1={'first':'sunday','second':'monday'}
+dict2={1:3,2:4}
+dict1.update(dict2)
+print(dict1)
