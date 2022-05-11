89b072e9d5b9cd8876fa5090ea815be7c9006c41
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2022-05-11 08:22:35 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,9 @@
+class cars:
+    def init_(self,a_name,model,types):
+        self.a_name=a_name
+        self.model=model
+        self.types=types
+
+car1=cars("Maruti","ciaz","sedan")
+c1=car1.model
+print('Model of car1=',c1)
